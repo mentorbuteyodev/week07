563bc99324840b012771092e8bb6770db3e2988e
Vincent-Kibet <kibetvincent931@gmail.com>
2025-08-28 10:58:58 +0300

    Docs: Trigger backend CI workflow

--- backend/product_service/app/main.py
+++ backend/product_service/app/main.py
@@ -31,6 +31,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
 
+# -- Extra imports
 from .db import Base, engine, get_db
 from .models import Product
 from .schemas import ProductCreate, ProductResponse, ProductUpdate, StockDeductRequest
